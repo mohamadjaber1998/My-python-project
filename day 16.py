5e763481862875f8b928f6d8aaaedd8ce7d7a3a7
mohamadjaber1998 <62432006+mohamadjaber1998@users.noreply.github.com>
2022-10-16 05:37:42 -0400

    Delete day 16.py

--- day 16.py
+++
@@ -1,6 +0,0 @@
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column('Pokemon Name', ['Pikachu', 'Hazer', 'Ismail'])
-table.add_column('Type', ['Dry', 'Human', 'Water'])
-table.align = 'c'
-print(table)
